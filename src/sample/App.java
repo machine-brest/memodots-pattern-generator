8a53d0de6c537f14caa8125e1092014b5633cb32
machine-brest <machine.brest@gmail.com>
2014-10-03 02:17:36 +0300

    Save changes

--- src/sample/App.java
+++ src/sample/App.java
@@ -4,9 +4,11 @@ import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -45,14 +47,32 @@ public class App extends Application
 			initContentPane(contentPane);
 
 			primaryStage.setScene(new Scene(root));
-			primaryStage.setResizable(false);
+			primaryStage.setResizable(true);
+			primaryStage.setWidth(400);
+			primaryStage.setHeight(550);
 			primaryStage.setTitle("MemoDOTS pattern generator");
 			primaryStage.show();
 		}
 
+		@FXML Group gridBackground;
+		@FXML Group levelPattern;
+		@FXML Group userPattern;
+
 		protected void initContentPane(Pane parent) {
+
 			// parent.setPrefSize(500, 800);
-			parent.autosize();
+			// parent.autosize();
+
+			byte gridXCount = 8;
+			byte gridYCount = 8;
+
+			for (byte col = 0; col < gridXCount; ++col)
+				for (byte row = 0; row < gridYCount; ++row) {
+					Circle dot = new Circle(col * 40.0, row * 40, 8.0);
+					dot.getStyleClass().add("background-dot");
+					gridBackground.getChildren().add(dot);
+					gridBackground.setMouseTransparent(true);
+				}
 		}
 
 		@FXML
